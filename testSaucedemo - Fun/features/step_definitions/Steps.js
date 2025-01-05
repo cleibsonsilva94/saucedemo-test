863ef443c1b6b456c6f865ac5d0a043ad20c7572
cleibsonsilva94 <cleibsonslima@gmail.com>
2025-01-05 01:09:12 -0300

    Missing add click

--- testSaucedemo - Fun/features/step_definitions/Steps.js
+++ testSaucedemo - Fun/features/step_definitions/Steps.js
@@ -2,14 +2,14 @@ const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
 const { Builder, By, until } = require('selenium-webdriver');
 const xpaths = require('../support/xpaths');
 let driver;
-const timeout = 30000;
+const timeout = 30000; // Definindo o timeout
 
 Before(async function () {
-    driver = await new Builder().forBrowser('chrome').build(); 
+  driver = await new Builder().forBrowser('chrome').build();
 });
 
 After(async function () {
-  await driver.quit(); 
+  await driver.quit();
 });
 
 Given('I am on the Swag Labs login page', async function () {
@@ -17,10 +17,12 @@ Given('I am on the Swag Labs login page', async function () {
 });
 
 When('I click on the Username field and type {string}', async function (username) {
+  await driver.wait(until.elementLocated(By.xpath(xpaths.XPATH_FOR_USERNAME_FIELD)), timeout);
   await driver.findElement(By.xpath(xpaths.XPATH_FOR_USERNAME_FIELD)).sendKeys(username);
 });
 
 When('I click on the Password field and type {string}', async function (password) {
+  await driver.wait(until.elementLocated(By.xpath(xpaths.XPATH_FOR_PASSWORD_FIELD)), timeout);
   await driver.findElement(By.xpath(xpaths.XPATH_FOR_PASSWORD_FIELD)).sendKeys(password);
 });
 

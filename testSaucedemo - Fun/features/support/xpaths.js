bd45e2ea9ec2ac7fcb0e84f14f79c7b9ebb9d497
cleibsonsilva94 <cleibsonslima@gmail.com>
2025-01-05 22:06:46 -0300

    Adding 1 path (from inventory) and updating the login button

--- testSaucedemo - Fun/features/support/xpaths.js
+++ testSaucedemo - Fun/features/support/xpaths.js
@@ -3,6 +3,7 @@ module.exports = {
   URL: "https://www.saucedemo.com/",
   XPATH_FOR_USERNAME_FIELD: "//input[@id='user-name']",
   XPATH_FOR_PASSWORD_FIELD: "//input[@id='password']",
-  XPATH_LOGIN_BUTTON: "//input[@id='login-button']",
+  XPATH_LOGIN_BUTTON: "//*[@id='login-button']",
+  XPATH_IVENTORY_HOME_PAGE: "(//div[@id='inventory_container'])[1]",
   URLHOME: "https://www.saucedemo.com/inventory.html"
 };
